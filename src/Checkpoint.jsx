1415c5b53eea87b945cbdb833ac3c0e3a8112aac
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-02-05 15:45:39 +0100

    Add files via upload

--- src/Checkpoint.jsx
+++ src/Checkpoint.jsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react";
 
 export function ImgAPI() {
-    const[user , setUser] = useState([])
+    const [user, setUser] = useState(null)
 
-    useEffect(()=> {
+    useEffect(() => {
         async function getPokemon() {
             const response = await fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
             const data = await response.json()
             setUser(data)
-            console.log(data);
+            console.log(data)
         }
+
         getPokemon()
     }, [])
-    return (
-         <div>
-            <p>{user.base_experience}</p>
-            <p>{user.abilities}</p>
-         </div>
-    )
 
+    return (
+        <div>
+            <img src={user.sprites.front_default} />
+          <h2>Name: {user.name}</h2>
+          <p>Base Experience: {user.base_experience}</p>
+          <h3>Abilities:</h3>
+          <ul>
+            {user.abilities.map((ability, index) => (
+              <li key={index}>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </div>
+    )
 }
 
